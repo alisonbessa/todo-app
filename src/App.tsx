c9d3b1106d914e7d78c426629c8737d1575d8235
alisonbessa <alisonbessa@gmail.com>
2025-07-01 12:09:45 -0300

    refactor: rename PageMain to PageHome and update import path in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import LayoutMain from "./pages/layout-main";
 import PageComponents from "./pages/page-components";
-import PageHome from "./pages/page-main";
+import PageHome from "./pages/page-home";
 
 export default function App() {
   return (
--- src/pages/page-main.tsx
+++
@@ -1,5 +0,0 @@
-import Container from "../components/container";
-
-export default function PageHome() {
-  return <Container>Home Page</Container>;
-}
