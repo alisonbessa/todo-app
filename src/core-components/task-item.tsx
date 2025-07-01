1c59a009b2ad18f6f74bad6bdd4e5591a17da538
alisonbessa <alisonbessa@gmail.com>
2025-07-01 12:02:47 -0300

    feat: implement task title update functionality in TaskItem component

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -11,6 +11,7 @@ import CheckIcon from "../assets/icons/check.svg?react";
 import InputText from "../components/input-text";
 import { type Task, TaskState } from "../models/task";
 import { cx } from "class-variance-authority";
+import useTask from "../hooks/use-task";
 
 interface TaskItemProps {
   task: Task;
@@ -20,7 +21,8 @@ export default function TaskItem({ task }: TaskItemProps) {
   const [isEditing, setIsEditing] = React.useState(
     task?.state === TaskState.Creating
   );
-  const [taskTitle, setTaskTitle] = React.useState("");
+  const [taskTitle, setTaskTitle] = React.useState(task.title || "");
+  const { updateTask } = useTask();
 
   function handleEditTask() {
     setIsEditing(true);
@@ -36,7 +38,9 @@ export default function TaskItem({ task }: TaskItemProps) {
 
   function handleSaveTask(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    console.log({ id: task.id, title: taskTitle });
+
+    updateTask(task.id, { title: taskTitle });
+
     setIsEditing(false);
   }
 
@@ -63,6 +67,7 @@ export default function TaskItem({ task }: TaskItemProps) {
       ) : (
         <form onSubmit={handleSaveTask} className="flex items-center gap-4">
           <InputText
+            value={taskTitle}
             className="flex-1"
             onChange={handleChangeTaskTitle}
             required
